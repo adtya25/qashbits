aaa5c37eddd806f61d0590f5e57ecaee17bf2746
adtya25 <79787334+adtya25@users.noreply.github.com>
2021-07-02 07:24:01 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Untuk Menjalankan Scrip
+pkg install php
+pkg install imagemagick
+pkg install tesseract
+*/
+
+$UA = "xxxxxxxxxx";
+
+$PHPSESSID = "xxxxxxxxxx";
+
+//URL SOLVEMEDIA PAPI CHALLENG
+
+$URL = "xxxxxxxxxx";
+
+
